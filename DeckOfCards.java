fd678142199e861d684bdf94520d33bcb846a5d5
pranay <pranaykawale500@gmail.com>
2021-11-17 12:46:20 +0530

    Added:Added plyers from console

--- DeckOfCards.java
+++ DeckOfCards.java
@@ -1,33 +1,54 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+* The DeckOfCards program 
+* simply  initialise the deck of cards game.
+*
+* @author  Pranay Kawale
+* @version 1.0
+* @since   2021-08-26 
+*/
+
 public class DeckOfCards {
-	int[] deck = new int [52];
-	private int rank, suit;
-	private static String[] suits = { "hearts", "spades", "diamonds", "clubs" };
+	 Scanner input=new Scanner(System.in);
+	    //creating Arraylist
+	    ArrayList<String> cardCollection = new ArrayList<>();
+	    /*
+	    In this I have created String type array of Suits and Ranks to store value.
+	     */
+	    public static String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+	    public static String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace"};
 
-	private static String[] ranks  = {  "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" ,"Ace"};
 
-	public DeckOfCards(int suit, int rank)
+	    public void initializeCards() {
+	        
+	    
+	        int totalCards = suits.length * ranks.length;
+	        System.out.println("Total no of card is : " + totalCards);
+	        for (int i = 0; i < suits.length; i++) {
+	            for (int j = 0; j < ranks.length; j++) {
+	                cardCollection.add(suits[i]+ranks[j]); //storing elements into arraylist
+	            }
+	        }
+	        System.out.println("Cards are: "+ cardCollection);
+	    }
 
-	{
-		this.rank=rank;
-		this.suit=suit;
+	    public void noOfPlayer(){
+	        System.out.println("enter the no. of player : ");
+	        int noOfPlayer=input.nextInt();
+	        if (noOfPlayer>=2 && noOfPlayer<=4){
+	            System.out.println(noOfPlayer+"players will play the game");
+	        }
+	        else
+	            System.out.println("min 2 and max 4 players can play the game");
+	    }
+	    public static void main(String[] args) {
+	        System.out.println("Welcome to decks of card problem");
+	        DeckOfCards deckOfCardMain = new DeckOfCards();
+	        deckOfCardMain.initializeCards();
+	        deckOfCardMain.noOfPlayer();
+	    }
 	}
-
-	public @Override String toString()
-	{
-		return ranks[rank] + " of " + suits[suit];
-	}
-
-
-
-	public int getRank() {
-		return rank;
-	}
-
-	public int getSuit() {
-		return suit;
-
-	}
-	
-}
